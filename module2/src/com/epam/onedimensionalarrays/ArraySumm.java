fa2706634f26af5f9fd899a1653c247c8fde80d0
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2020-12-26 22:07:42 +0300

    deleted module2

--- module2/src/com/epam/onedimensionalarrays/ArraySumm.java
+++ module2/src/com/epam/onedimensionalarrays/ArraySumm.java
@@ -5,17 +5,16 @@ public class ArraySumm {
 
     public static void sumElements() {
         int arrayLength = 10;
-        int [] array = new int[arrayLength];
-        for(int i = 0; i < array.length; i++) {
-            array[i] = (int)(Math.random() * 10);
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
         }
         int i = 0;
         int sumElements = 0;
         if (array[i] % DIVIDER == 0) {
-            sumElements =+ array[i];
+            sumElements = +array[i];
             i++;
         }
         System.out.println("Sum of elements  = " + sumElements);
     }
 }
-
