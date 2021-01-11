887c44e74204c645d0b2b557fbba60e927c3e11f
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-01-11 16:19:37 +0300

    arrays of arrays

---
+++ module2/src/com/epam/arraysofarrays/TaskTwoLauncher.java
@@ -0,0 +1,7 @@
+package com.epam.arraysofarrays;
+
+public class TaskTwoLauncher {
+    public static void main(String[] args) {
+        System.out.println(SquareMatrixOrderN.squareMatrixOrderN(6));
+        }
+    }
