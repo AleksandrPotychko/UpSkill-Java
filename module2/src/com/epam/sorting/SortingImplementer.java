78fe07d302984e496b412573c7af173b5f8562a1
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-03-07 23:16:13 +0300

    Sorting all tasks.

--- module2/src/com/epam/sorting/SortingImplementer.java
+++ module2/src/com/epam/sorting/SortingImplementer.java
@@ -1,6 +1,24 @@
 package com.epam.sorting;
 
 public class SortingImplementer {
+
+    //task 1
+    public static int[] InsertArrayIntoArray(int[] longArray, int[] shortArray, int insertIndexIntoArray) {
+        int [] summaryArray = new int[longArray.length + shortArray.length];
+
+        for (int i = 0; i < (summaryArray.length - shortArray.length + 1); i ++) {
+            if ( i < insertIndexIntoArray) {
+                summaryArray[i] = longArray[i];
+            } else if (i == insertIndexIntoArray) {
+                for (int j = 0; j < shortArray.length; j++) {
+                    summaryArray[i + j] = shortArray[j];
+                }
+            } else if (i > insertIndexIntoArray) {
+                summaryArray[i + shortArray.length - 1] = longArray[i - 1];
+            }
+        } return  summaryArray;
+    }
+
     // task 2
     public static int[] applySelectionSort(int[] array) {
         for (int i = 0; i < array.length-1; i++) {
@@ -29,6 +47,30 @@ public class SortingImplementer {
                 isSorted = false;
             }
         }
+        } return array;
+    }
+
+    //task 4
+    public static int[] applyInsertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; array[j - 1] > array[j]; j--) {
+                int temp = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = temp;
+            }
+        } return array;
+    }
+
+    //task 5
+    public static int[] applyShellSort(int[] array) {
+        for (int step = array.length / 2; step > 0 ; step /= 2) {
+            for (int i = step; i < array.length; i++) {
+                for (int j = i - step; j >= 0 && array[j] > array[j + step]; j -= step) {
+                    int temp = array[j];
+                    array[j] = array[j + step];
+                    array[j + step] = temp;
+                }
+            }
         } return array;
     }
 }
--- module2/src/com/epam/sorting/TaskLauncher.java
+++ module2/src/com/epam/sorting/TaskLauncher.java
@@ -4,8 +4,15 @@ import java.util.Arrays;
 
 public class TaskLauncher {
     private static final int LENGTH_ARRAY = 10;
+    private static final int SHORT_ARRAY_LENGTH = 5;
+    private static final int INSERT_INDEX_INTO_ARRAY = 3;
 
     public static void main(String[] args) {
+        //task 1
+        System.out.println(Arrays.toString(SortingImplementer.InsertArrayIntoArray(ArrayInitializer.
+                buildArray(LENGTH_ARRAY), ArrayInitializer.
+                buildArray(SHORT_ARRAY_LENGTH), INSERT_INDEX_INTO_ARRAY)));
+
         //task 2
         System.out.println(Arrays.toString(SortingImplementer.
                 applySelectionSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
@@ -13,5 +20,13 @@ public class TaskLauncher {
         //task 3
         System.out.println(Arrays.toString(SortingImplementer.
                 applyBubbleSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
+
+        // task 4
+        System.out.println(Arrays.toString(SortingImplementer.
+                applySelectionSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
+
+        // task 5
+        System.out.println(Arrays.toString(SortingImplementer.
+                applyShellSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
     }
 }
