d32682e4c4a4dc9d84d8493ca029d56e5d0d71ee
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-03-10 11:05:23 +0300

    Sorting answers to comments1

--- module2/src/com/epam/sorting/ArraySorter.java
+++ module2/src/com/epam/sorting/ArraySorter.java
@@ -1,26 +1,26 @@
 package com.epam.sorting;
 
-public class SortingImplementer {
+public class ArraySorter {
 
     //task 1
-    public static int[] InsertArrayIntoArray(int[] longArray, int[] shortArray, int insertIndexIntoArray) {
-        int [] summaryArray = new int[longArray.length + shortArray.length];
+    public static void insertArrayIntoArray(int[] array, int[] insertionArray, int insertIndexIntoArray) {
+        int [] summaryArray = new int[array.length + insertionArray.length];
 
-        for (int i = 0; i < (summaryArray.length - shortArray.length + 1); i ++) {
+        for (int i = 0; i < (summaryArray.length - insertionArray.length + 1); i ++) {
             if ( i < insertIndexIntoArray) {
-                summaryArray[i] = longArray[i];
+                summaryArray[i] = array[i];
             } else if (i == insertIndexIntoArray) {
-                for (int j = 0; j < shortArray.length; j++) {
-                    summaryArray[i + j] = shortArray[j];
+                for (int j = 0; j < insertionArray.length; j++) {
+                    summaryArray[i + j] = insertionArray[j];
                 }
             } else if (i > insertIndexIntoArray) {
-                summaryArray[i + shortArray.length - 1] = longArray[i - 1];
+                summaryArray[i + insertionArray.length - 1] = array[i - 1];
             }
-        } return  summaryArray;
+        }
     }
 
     // task 2
-    public static int[] applySelectionSort(int[] array) {
+    public static void applySelectionSort(int[] array) {
         for (int i = 0; i < array.length-1; i++) {
             int minimalNumber = i;
             for (int j = i + 1; j < array.length; j++) {
@@ -31,38 +31,38 @@ public class SortingImplementer {
             int temp = array[i];
             array[i] = array[minimalNumber];
             array[minimalNumber] = temp;
-        } return array;
+        }
     }
 
     //task 3
-    public static int[] applyBubbleSort(int[] array) {
+    public static void applyBubbleSort(int[] array) {
         boolean isSorted = false;
         while (!isSorted) {
             isSorted = true;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < array[i - 1]) {
-                int temp = array[i - 1];
-                array[i - 1] = array[i];
-                array[i] = temp;
-                isSorted = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    isSorted = false;
+                }
             }
         }
-        } return array;
     }
 
     //task 4
-    public static int[] applyInsertionSort(int[] array) {
+    public static void applyInsertionSort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             for (int j = i; array[j - 1] > array[j]; j--) {
                 int temp = array[j - 1];
                 array[j - 1] = array[j];
                 array[j] = temp;
             }
-        } return array;
+        }
     }
 
     //task 5
-    public static int[] applyShellSort(int[] array) {
+    public static void applyShellSort(int[] array) {
         for (int step = array.length / 2; step > 0 ; step /= 2) {
             for (int i = step; i < array.length; i++) {
                 for (int j = i - step; j >= 0 && array[j] > array[j + step]; j -= step) {
@@ -71,6 +71,6 @@ public class SortingImplementer {
                     array[j + step] = temp;
                 }
             }
-        } return array;
+        }
     }
 }
--- module2/src/com/epam/sorting/TaskLauncher.java
+++ module2/src/com/epam/sorting/TaskLauncher.java
@@ -1,17 +1,18 @@
 package com.epam.sorting;
 
-import java.util.Arrays;
 
 public class TaskLauncher {
     private static final int LENGTH_ARRAY = 10;
     private static final int SHORT_ARRAY_LENGTH = 5;
     private static final int INSERT_INDEX_INTO_ARRAY = 3;
+    private static final int MIN_BORDER = 0;
+    private static final int MAX_BORDER = 1000;
 
     public static void main(String[] args) {
         //task 1
-        System.out.println(Arrays.toString(SortingImplementer.InsertArrayIntoArray(ArrayInitializer.
-                buildArray(LENGTH_ARRAY), ArrayInitializer.
-                buildArray(SHORT_ARRAY_LENGTH), INSERT_INDEX_INTO_ARRAY)));
+        ArraySorter.insertArrayIntoArray(ArrayInitializer.
+                buildArray(LENGTH_ARRAY, MIN_BORDER, MAX_BORDER), ArrayInitializer.
+                buildArray(SHORT_ARRAY_LENGTH, MIN_BORDER, MAX_BORDER), INSERT_INDEX_INTO_ARRAY);
 
         //task 2
         System.out.println(Arrays.toString(SortingImplementer.
