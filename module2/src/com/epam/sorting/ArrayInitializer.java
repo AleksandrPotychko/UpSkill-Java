b775d22793d8012ae76a54beb09f345c48c1b14c
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-03-10 10:37:32 +0300

    Sorting answers to comments.

--- module2/src/com/epam/sorting/ArrayInitializer.java
+++ module2/src/com/epam/sorting/ArrayInitializer.java
@@ -1,10 +1,10 @@
 package com.epam.sorting;
 
 public class ArrayInitializer {
-    public static int[] buildArray(int lengthArray) {
+    public static int[] buildArray(int lengthArray, int minBorder, int maxBorder ) {
         int[] array = new int[lengthArray];
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 1000));
+            array[i] = ((int) (Math.random() * maxBorder) - minBorder);
         } return array;
     }
 }
--- module2/src/com/epam/sorting/TaskLauncher.java
+++ module2/src/com/epam/sorting/TaskLauncher.java
@@ -6,27 +6,25 @@ public class TaskLauncher {
     private static final int LENGTH_ARRAY = 10;
     private static final int SHORT_ARRAY_LENGTH = 5;
     private static final int INSERT_INDEX_INTO_ARRAY = 3;
+    private static final int MIN_BORDER = 0;
+    private static final int MAX_BORDER = 1000;
 
     public static void main(String[] args) {
         //task 1
-        System.out.println(Arrays.toString(SortingImplementer.InsertArrayIntoArray(ArrayInitializer.
-                buildArray(LENGTH_ARRAY), ArrayInitializer.
-                buildArray(SHORT_ARRAY_LENGTH), INSERT_INDEX_INTO_ARRAY)));
+        ArraySorter.insertArrayIntoArray(ArrayInitializer.
+                buildArray(LENGTH_ARRAY, MAX_BORDER, MIN_BORDER), ArrayInitializer.
+                buildArray(SHORT_ARRAY_LENGTH, MAX_BORDER, MIN_BORDER), INSERT_INDEX_INTO_ARRAY);
 
         //task 2
-        System.out.println(Arrays.toString(SortingImplementer.
-                applySelectionSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
+        ArraySorter.applySelectionSort(ArrayInitializer.buildArray(LENGTH_ARRAY, MAX_BORDER, MIN_BORDER));
 
         //task 3
-        System.out.println(Arrays.toString(SortingImplementer.
-                applyBubbleSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
+        ArraySorter.applyBubbleSort(ArrayInitializer.buildArray(LENGTH_ARRAY, MAX_BORDER, MIN_BORDER));
 
         // task 4
-        System.out.println(Arrays.toString(SortingImplementer.
-                applySelectionSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
+        ArraySorter.applyInsertionSort(ArrayInitializer.buildArray(LENGTH_ARRAY, MAX_BORDER, MIN_BORDER));
 
         // task 5
-        System.out.println(Arrays.toString(SortingImplementer.
-                applyShellSort(ArrayInitializer.buildArray(LENGTH_ARRAY))));
+        ArraySorter.applyShellSort(ArrayInitializer.buildArray(LENGTH_ARRAY, MAX_BORDER, MIN_BORDER));
     }
 }
