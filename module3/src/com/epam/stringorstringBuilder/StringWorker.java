3cf62c2cdc234aae7adbc7d48a83386340d51123
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-03-31 00:41:03 +0300

    String builder ten tasks, answer to comments.

--- module3/src/com/epam/stringorstringBuilder/StringWorker.java
+++ module3/src/com/epam/stringorstringBuilder/StringWorker.java
@@ -13,59 +13,64 @@ public class StringWorker {
                 if (lengthOfSpaces > maxLengthOfSpaces) {
                     maxLengthOfSpaces = lengthOfSpaces;
                 }
-            } else lengthOfSpaces = 0;
+            } else {
+                lengthOfSpaces = 0;
+            }
         }
         return maxLengthOfSpaces;
     }
 
     //task 2
-    public static String insertBAfterAch(String text) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == 'a') {
-                stringBuilder.append("ab");
-            } else if ((text.charAt(i) == 'A')) {
-                stringBuilder.append("AB");
-            } else {
-                stringBuilder.append(text.charAt(i));
-            }
-        }
-        return stringBuilder.toString();
+    public static String insertBAfterA(String text, String regex, String replacement) {
+        return text.replaceAll(regex, replacement);
     }
 
     //task 3
     public static boolean isPalindrome(String word) {
-        int wordLength = word.length();
-        for (int i = 0; i < (wordLength / 2); i++) {
-            if (word.charAt(i) != word.charAt(wordLength - i - 1)) {
-                return false;
-            }
-        }
-        return true;
+        return word.equals(new StringBuilder(word).reverse().toString());
     }
 
     //task 4
-    public static String convertInfomaticaToCake(String word) {
-        String cake = String.valueOf(word.charAt(word.indexOf('т')));
-        cake += String.valueOf(word.charAt(word.indexOf('о')));
-        cake += String.valueOf(word.charAt(word.indexOf('р')));
-        cake += String.valueOf(word.charAt(word.indexOf('т')));
-        return cake;
+    public static String convertInformaticaToTort(String word) {
+        final char LETTER_T = 't';
+        final char LETTER_O = 'o';
+        final char LETTER_R = 'r';
+        StringBuilder result = new StringBuilder();
+        final char[] characters = word.toCharArray();
+        for (char character : characters) {
+            if (character == LETTER_T) {
+                result.append(character);
+            }
+        }
+        for (char character : characters) {
+            if (character == LETTER_O) {
+                result.append(character);
+            }
+        }
+        for (char character : characters) {
+            if (character == LETTER_R) {
+                result.append(character);
+            }
+        }
+        for (char character : characters) {
+            if (character == LETTER_T) {
+                result.append(character);
+            }
+        }
+        return result.toString();
     }
 
     //task 5
-    public static int calculateTheA(String text) {
-        int count = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == 'a' || (text.charAt(i) == 'A')) {
-                count++;
+    public static int calculateTheA(String text, String letterA) {
+        int occurrencesOfCharacter = 0;
+        for (int i = text.indexOf(letterA); i != -1; i = text.indexOf(letterA, i + 1)) {
+            occurrencesOfCharacter++;
             }
-        }
-        return count;
+        return occurrencesOfCharacter;
     }
 
     //task 6
-    public static String charactersDoubler(String text) {
+    public static String repeatEveryCharacterTwice(String text) {
         StringBuilder stringBuilder = new StringBuilder();
         int numberOfRepetitions = 2;
         for (int i = 0; i < text.length(); i++) {
@@ -78,75 +83,61 @@ public class StringWorker {
 
     //task 7
     public static String deleteRepeatedCharactersAndSpaces(String text) {
-        String textWithOutSpaces = deleteSpaces(text);
-        return deleteRepeatedCharacters(textWithOutSpaces);
-    }
-
-    private static String deleteSpaces(String text) {
-        StringBuilder stringBuilder = new StringBuilder(text);
-        for (int i = 0; i < stringBuilder.length(); i++) {
-            if (stringBuilder.charAt(i) == ' ') {
-                stringBuilder.deleteCharAt(i);
+        char[] characters = text.toCharArray();
+        StringBuilder modifiedStr = new StringBuilder();
+        for (char ch : characters) {
+            if (ch != ' ' && modifiedStr.indexOf(String.valueOf(ch)) == -1) {
+                modifiedStr.append(ch);
             }
         }
-        return stringBuilder.toString();
-    }
-
-    private static String deleteRepeatedCharacters(String text) {
-        StringBuilder stringBuilder = new StringBuilder(text);
-        for (int i = 0; i < stringBuilder.length(); i++) {
-            char character = stringBuilder.charAt(i);
-            for (int j = i + 1; j < stringBuilder.length(); j++) {
-                if (stringBuilder.charAt(j) == character) {
-                    stringBuilder.deleteCharAt(j);
-                }
-            }
-        }
-        return stringBuilder.toString();
+        return modifiedStr.toString();
     }
 
     //task 8
-    public static int calculateLargestNumberOfWord(String text) {
-        int lengthOfWord = 0;
-        int maxLengthOfWord = 0;
-        final char[] characters = text.toCharArray();
-        for (int i = 0; i < characters.length; i++) {
-            if (characters[i] != ' ') {
-                lengthOfWord++;
-                if (lengthOfWord > maxLengthOfWord) {
-                    maxLengthOfWord = lengthOfWord;
-                }
-            } else lengthOfWord = 0;
+    public static String calculateLargestNumberOfWord(String text) {
+        String[] words = text.split(" ");
+        String longestWord = "";
+        for (String word: words) {
+            if (word.length() > longestWord.length())
+                longestWord = word;
         }
-        return maxLengthOfWord;
+        return longestWord ;
     }
 
     //task 9
-    public static void calculateLowerAndUpperCase(String text) {
-        int counterLowerCase = 0;
-        int counterUpperCase = 0;
+    public static int[] calculateLowerAndUpperCase(String text) {
+        int[] resultArray = new int[2];
+        final char LOWER_CASE_A = 'a';
+        final char LOWER_CASE_Z = 'z';
+        final char UPPER_CASE_A = 'A';
+        final char UPPER_CASE_Z = 'Z';
         final char[] characters = text.toCharArray();
-        for (int i = 0; i < characters.length; i++) {
-            if (characters[i] >= 'a' && characters[i] <= 'z') {
-                counterLowerCase++;
+        for (char character : characters) {
+            if (character >= LOWER_CASE_A && character <= LOWER_CASE_Z) {
+                resultArray[0]++;
             }
-            if (characters[i] >= 'A' && characters[i] <= 'Z') {
-                counterUpperCase++;
+            if (character >= UPPER_CASE_A && character <= UPPER_CASE_Z) {
+                resultArray[1]++;
             }
         }
-        System.out.println("Number of lowercase letters = " + counterLowerCase);
-        System.out.println("Number of uppercase letters = " + counterUpperCase);
+        return resultArray;
     }
 
     //task 10
     public static int calculateTheSentences(String text) {
-        int counterSentences = 0;
-        final char[] characters = text.toCharArray();
-        for (int i = 0; i < characters.length; i++) {
-            if (characters[i] == '.' || characters[i] == '!' || characters[i] == '?') {
-                counterSentences++;
-            }
+        int occurrencesOfCharacter = 0;
+        final char DOT = '.';
+        final char EXCLAMATION_MARK = '!';
+        final char QUESTION_MARK = '?';
+        for (int i = text.indexOf(DOT); i != -1; i = text.indexOf(DOT, i + 1)) {
+            occurrencesOfCharacter++;
         }
-        return counterSentences;
+        for (int i = text.indexOf(EXCLAMATION_MARK); i != -1; i = text.indexOf(EXCLAMATION_MARK, i + 1)) {
+            occurrencesOfCharacter++;
+        }
+        for (int i = text.indexOf(QUESTION_MARK); i != -1; i = text.indexOf(QUESTION_MARK, i + 1)) {
+            occurrencesOfCharacter++;
+        }
+        return occurrencesOfCharacter;
     }
 }
--- module3/src/com/epam/stringorstringBuilder/TaskLauncher.java
+++ module3/src/com/epam/stringorstringBuilder/TaskLauncher.java
@@ -1,10 +1,14 @@
 package com.epam.stringorstringBuilder;
 
+import java.util.Arrays;
+
 public class TaskLauncher {
     private final static String TEXT_FOR_THE_TASK = " world A ?12  and!   875 MILk a     345  sugar 11 or salt5world. ";
     private final static String WORD_PALINDROME = "level";
-    private final static String INFORMATICS = "информатика";
+    private final static String INFORMATICS = "informatica";
     private final static String TASK_SEVEN = "abc cde def";
+    private final static String LETTER_A = "a";
+    private final static String LETTERS_AB = "ab";
 
 
     public static void main(String[] args) {
@@ -12,19 +16,19 @@ public class TaskLauncher {
         System.out.println(StringWorker.calculateLargestNumberOfSpaces(TEXT_FOR_THE_TASK));
 
         //task 2
-        System.out.println(StringWorker.insertBAfterAch(TEXT_FOR_THE_TASK));
+        System.out.println(StringWorker.insertBAfterA(TEXT_FOR_THE_TASK, LETTER_A, LETTERS_AB));
 
         //task 3
         System.out.println(StringWorker.isPalindrome(WORD_PALINDROME));
 
         //task 4
-        System.out.println(StringWorker.convertInfomaticaToCake(INFORMATICS));
+        System.out.println(StringWorker.convertInformaticaToTort(INFORMATICS));
 
         //task 5
-        System.out.println(StringWorker.calculateTheA(TEXT_FOR_THE_TASK));
+        System.out.println(StringWorker.calculateTheA(TEXT_FOR_THE_TASK, LETTER_A));
 
         //task 6
-        System.out.println(StringWorker.charactersDoubler(INFORMATICS));
+        System.out.println(StringWorker.repeatEveryCharacterTwice(INFORMATICS));
 
         //task 7
         System.out.println(StringWorker.deleteRepeatedCharactersAndSpaces(TASK_SEVEN));
@@ -33,7 +37,7 @@ public class TaskLauncher {
         System.out.println(StringWorker.calculateLargestNumberOfWord(TEXT_FOR_THE_TASK));
 
         //task 9
-        StringWorker.calculateLowerAndUpperCase(TEXT_FOR_THE_TASK);
+        System.out.println(Arrays.toString(StringWorker.calculateLowerAndUpperCase(TEXT_FOR_THE_TASK)));
 
         //task 10
         System.out.println(StringWorker.calculateTheSentences(TEXT_FOR_THE_TASK));
