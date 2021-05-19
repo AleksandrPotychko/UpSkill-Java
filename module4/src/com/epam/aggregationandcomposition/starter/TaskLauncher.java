297dd693d7a3c6763a8632082214ce17e80d02bb
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-05-19 10:54:58 +0300

    module 4, aggregation five tasks with correct TaskLauncher.

--- module4/src/com/epam/aggregationandcomposition/starter/TaskLauncher.java
+++ module4/src/com/epam/aggregationandcomposition/starter/TaskLauncher.java
@@ -15,8 +15,10 @@ public class TaskLauncher {
     public static String COUNTRY_NAME = "Minsk";
     public static String WORD = "Heading";
     public static String NAME_BANK = "Belarusbank";
+    public static String NAME_TRAVEL_AGENCY = "Serious Sam";
     public static final int STATE_AREA = 15000;
     public static final int ACCOUNT_ID = 5;
+    public static final int DAYS = 21;
 
     public static void main(String[] args) {
         // Task 1
@@ -50,5 +52,20 @@ public class TaskLauncher {
         System.out.println(bank.findAccountById(ACCOUNT_ID));
         bank.getClients().get(1).sortByBalance();
         System.out.println(bank.getClients().get(1).getAccounts());
+
+        // Task 5
+        TravelAgency travelAgency = new TravelAgency(NAME_TRAVEL_AGENCY, TripInitializer.createTrips());
+        System.out.println(travelAgency.findTripsByFeeding(Feeding.ALL_INCLUSIVE));
+        System.out.println(travelAgency.findTripsByType(TypeOfTrip.SANATORIUM));
+        System.out.println(travelAgency.findTripsByTransport(Transport.SHIP));
+        System.out.println(travelAgency.findTripsByTypeAndDays(TypeOfTrip.CRUISE, DAYS));
+        travelAgency.sortByDays();
+        System.out.println(travelAgency);
+        travelAgency.sortByTransport();
+        System.out.println(travelAgency);
+        travelAgency.sortByFeeding();
+        System.out.println(travelAgency);
+        travelAgency.sortByTypeOfTrip();
+        System.out.println(travelAgency);
     }
 }
