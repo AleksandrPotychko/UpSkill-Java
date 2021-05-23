83456f53f7a81d4bb087995aaa3a900a3a0a1907
PotychkoAlexsander <p.minsk.potychko@gmail.com>
2021-05-24 00:04:23 +0300

    Module 5 2 tasks.

--- module5/src/com/epam/starter/TaskLauncher.java
+++ module5/src/com/epam/starter/TaskLauncher.java
@@ -1,14 +1,18 @@
 package com.epam.starter;
 
 import com.epam.entity.Directory;
+import com.epam.entity.Payment;
 import com.epam.unit.FileInitializer;
+import com.epam.unit.ProductInitializer;
 
 public class TaskLauncher {
     private static String directoryName = "Folder";
     private static final String FILE_NAME = "Empty file";
     private static final String SECOND_FILE_NAME = "Siri";
+    private static final String BUYER_NAME = " Wally";
 
     public static void main(String[] args) {
+
         // Task 1
         Directory directory = new Directory(directoryName,
                 FileInitializer.createFiles(), FileInitializer.createTextFiles());
@@ -19,5 +23,17 @@ public class TaskLauncher {
         System.out.println(directory);
         directory.getTextFiles().get(1).appendTextInFile("Lalalend");
         System.out.println(directory);
+
+        // Task 2
+        Payment payment = new Payment(BUYER_NAME, new Payment.ShoppingBasket());
+        System.out.println(payment);
+        payment.getShoppingBasket().addProduct(ProductInitializer.createProducts().get(0));
+        System.out.println(payment);
+        payment.getShoppingBasket().addProduct(ProductInitializer.createProducts().get(1));
+        System.out.println(payment);
+        payment.getShoppingBasket().addProduct(ProductInitializer.createProducts().get(2));
+        System.out.println(payment);
+        payment.getShoppingBasket().deleteProduct(ProductInitializer.createProducts().get(0));
+        System.out.println(payment);
     }
 }
